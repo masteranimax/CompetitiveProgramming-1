dcff3b34117fb23dd6106965444a678310c058f7
Aniket <43602527+masteranimax@users.noreply.github.com>
2020-10-01 22:06:41 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ int main()
 {
 
     int a[5] = {1, 3, 7 , 8 , 9};
-    cout << upper_bound(a, a+5, 7)-a << endl;
-    cout << lower_bound(a, a+5, 1)-a << endl;
+    cout << a[upper_bound(a, a+5, 7)-a] << endl;
+    cout << a[lower_bound(a, a+5, 1)-a] << endl;
     return 0;
 }
